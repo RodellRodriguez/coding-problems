0b0cb5e26c9dfce87205afbfec628ab2df0e8858
RodellRodriguez <rodriguez.rodell53@gmail.com>
2018-09-10 00:35:21 -0400

    Added leetcode solution

--- leetcode/find_all_anagrams_in_a_string.py
+++ leetcode/find_all_anagrams_in_a_string.py
@@ -3,6 +3,10 @@
 2 solutions. Solution 1 is my solution but it timed out. Time complexity is O(n^2)
 and it's too slow.
 
+Solution 2 is similar to solution 1. The biggest difference is that Counter
+is not being created at every iteration. Each time you create Counter, thats O(n)
+thus reducing the time complexity of the solution to O(n)
+
 """
 
 
@@ -25,4 +29,24 @@ class Solution(object):
                 res.append(l)
             l += 1
             r += 1
+        return res
+
+
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        res = []
+        pCounter = Counter(p)
+        sCounter = Counter(s[:len(p)-1])
+        for i in range(len(p)-1,len(s)):
+            sCounter[s[i]] += 1   # include a new char in the window
+            if sCounter == pCounter:    # This step is O(1)
+                res.append(i-len(p)+1)   # append the starting index
+            sCounter[s[i-len(p)+1]] -= 1   # decrease the count of oldest char in the window
+            if sCounter[s[i-len(p)+1]] == 0:
+                del sCounter[s[i-len(p)+1]]   # remove the count if it is 0
         return res
