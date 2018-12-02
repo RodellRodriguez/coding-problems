37c951b86df8ffa616bf2d6b09fa4fffc6ca7071
Rodell Rodriguez <rodriguez.rodell53@gmail.com>
2018-12-02 00:56:57 -0500

    Path Sum III

---
+++ leetcode/path_sum_III.py
@@ -0,0 +1,35 @@
+""" https://leetcode.com/problems/path-sum-iii/
+
+Brute Force:
+
+Go through each node and find all the possible paths using each node as the parent
+. If there is a match then return 1
+
+This is a preorder DFS traversal.
+
+Worst case time complexity is O(n^2) if the binary tree is single sided
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: int
+        """
+        if root:
+            return self.find_paths(root, sum) + self.pathSum(root.left, sum) + self.pathSum(root.right, sum)
+        return 0
+
+    def find_paths(self, root, target):
+        if root:
+            return int(root.val == target) + self.find_paths(root.left, target-root.val) + self.find_paths(root.right, target-root.val)
+        return 0
